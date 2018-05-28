b8835617948750e1fc477a71bc50fbac85cab682
Richard Williams <richard.williams2@manchester.ac.uk>
2018-05-28 22:35:21 +0100

    latest changes

--- app/assets/weather.php
+++ app/assets/weather.php
@@ -3,8 +3,8 @@ session_start();
 
 // Get the place to check - 352857 is nlw
 $place=!empty($_GET['place']) ? trim($_GET['place']) : "352827";
-if(!isset($_SESSION["p".$place])) {
 
+if(!isset($_SESSION["p".$place]) || (time()/3600 - $_SESSION["t".$place])>1) {
     $host = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" . $place . "?res=3hourly&key=" . getenv('MET_OFFICE_KEY');
 
     $ch = curl_init();
@@ -14,6 +14,7 @@ if(!isset($_SESSION["p".$place])) {
     // curl_setopt($ch, CURLOPT_VERBOSE, true);
 
     $_SESSION["p".$place] = curl_exec($ch);
+    $_SESSION["t".$place] = time()/3600;
 }
 
 header('Content-Type: application/json');
