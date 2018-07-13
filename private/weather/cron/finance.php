5c7ab4fc9bbf6dbda5309f45000ed3f9c35ea87e
Richard Williams <richard.williams2@manchester.ac.uk>
2018-07-13 07:53:45 +0100

    First update at 9 instead of 8

--- private/weather/cron/finance.php
+++ private/weather/cron/finance.php
@@ -8,7 +8,7 @@ $data = getAllFinancialData();
 
 $notifications = array();
 $queries = array();
-$oclock08 = date("H",strtotime("today 8 am Europe/London"));
+$oclock09 = date("H",strtotime("today 9 am Europe/London"));
 $oclock12 = date("H",strtotime("today 12 pm Europe/London"));
 $oclock17 = date("H",strtotime("today 5 pm Europe/London"));
 $oclock20 = date("H",strtotime("today 8 pm Europe/London"));
@@ -16,7 +16,7 @@ for($i=0; $i < count($data); $i++) {
     $isUpdatedToday = $data[$i]['isUpdatedToday'] ? 'TRUE' : 'FALSE';
     $changeToday = $data[$i]['changeToday'] == '?' ? "NULL" : $data[$i]['changeToday'];
     $changeYear = $data[$i]['changeYear'] == '?' ? "NULL" : $data[$i]['changeYear'];
-    if($data[$i]['oldPrice']!=$data[$i]['newPrice'] && (date('H')==$oclock08 || date('H')==$oclock12 || date('H')==$oclock17  )) {
+    if($data[$i]['oldPrice']!=$data[$i]['newPrice'] && (date('H')==$oclock09 || date('H')==$oclock12 || date('H')==$oclock17  )) {
         $notifications[] = $data[$i]['name'] . ' was ' . $data[$i]['oldPrice'] . ' is now ' . $data[$i]['newPrice'] . ' (' . $changeToday . '%).';
     } else if(!$isUpdatedToday && date('H')==$oclock20) {
         $notifications[] = $data[$i]['name'] . ' day change is unreported.';
